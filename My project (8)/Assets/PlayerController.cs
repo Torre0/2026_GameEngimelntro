74eb525c861013e9f882c0ee0511119ad462d261
Torre0 <obo0512obo@gmail.com>
2026-03-18 16:20:19 +0900

    Add : 플레이어 기본 움직임

---
+++ My project (8)/Assets/PlayerController.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    private Vector2 movelnput;
+    private float moveSpeed = 8f;
+    private float JumpForce = 9f;
+    private Rigidbody2D rb;
+    private Animator myAnimator;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        myAnimator.SetBool("move", false);
+    }
+    public void OnMove(InputValue value)
+    {
+        movelnput = value.Get<Vector2>();
+    }
+    public void OnJump(InputValue value)
+    {
+        if(value.isPressed) {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, JumpForce);
+        }
+    }
+    void Update()
+    {
+        if (movelnput.x > 0) {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if (movelnput.x < 0) {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        if(movelnput.magnitude > 0) {
+            myAnimator.SetBool("move", true);
+        }
+        else {
+            myAnimator.SetBool("move", false);
+        }
+        transform.Translate(Vector3.right * moveSpeed * movelnput.x * Time.deltaTime);
+    }
+}
