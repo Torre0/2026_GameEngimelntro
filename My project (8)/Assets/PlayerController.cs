88ca2f40647cd7d3baab2d3668a923f025f87d93
Torre0 <obo0512obo@gmail.com>
2026-03-25 16:53:05 +0900

    Add : 플레이어 기본 움직임

--- My project (8)/Assets/PlayerController.cs
+++ My project (8)/Assets/PlayerController.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
@@ -40,5 +41,18 @@ public class NewMonoBehaviourScript : MonoBehaviour
             myAnimator.SetBool("move", false);
         }
         transform.Translate(Vector3.right * moveSpeed * movelnput.x * Time.deltaTime);
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "Death")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        else
+        {
+            SceneManager.LoadScene("PlayScene_" + collision.name);
+        }
     }
 }
